8224e310a4558b5129618ad127f2d41c7bc1edd1
Stas <stas.vorobjev2001@gmail.com>
2020-04-23 17:46:08 +0300

    Fixed mtm fields

--- order/api/serializers.py
+++ order/api/serializers.py
@@ -3,8 +3,8 @@ from rest_framework import serializers
 from menu.api.serializers import DishListSerializer, DishNameListSerializer
 
 class OrderSerializer(serializers.HyperlinkedModelSerializer):
-    orderList = DishListSerializer(many=True)
-    order_list = DishNameListSerializer(many=True)
+    orderList = DishListSerializer(many=True, read_only=True)
+    order_list = DishNameListSerializer(many=True, read_only=True)
 
     class Meta:
         model = Order
